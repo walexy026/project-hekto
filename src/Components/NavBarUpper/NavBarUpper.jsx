eba1de1452b753e3f6eb332d33fc58007ba654dc
olawale <jao.olaniyi@gmail.com>
2022-10-07 13:54:32 +0100

    navBarUpper

--- src/Components/NavBarUpper/NavBarUpper.jsx
+++ src/Components/NavBarUpper/NavBarUpper.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './NavBarUpper.css'
+import mailBox from '../../Assets/mailBox.svg';
 
 const NavBarUpper = () => {
   return (
-    <div>NavBarUpper</div>
+   <nav>
+    <div className='contactAddress'>
+      <img src={mailBox} alt="" />
+    <p>mhhasanul@gmail.com</p>
+    <p>(12345)67890</p>
+    </div>
+    <div className='options'>
+    <p>English</p>
+    <p>USD</p>
+    <p>Login</p>
+    <p>Wishlist</p>
+    </div>
+   </nav>
   )
 }
 
