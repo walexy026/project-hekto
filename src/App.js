a912617bd8f0a7b7020941970cb145b4648479fe
olawale <jao.olaniyi@gmail.com>
2022-10-07 11:23:12 +0100

    setting up the environment

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {Route} from 'react-router-dom'
+function App() {
+  return (
+    <div className="App">
+     <h1>welcome</h1>
+    </div>
+  );
+}
+
+export default App;
