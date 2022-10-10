2b714722f32ccf0a16f58deba993f4b43e1996e6
olawale <jao.olaniyi@gmail.com>
2022-10-10 10:56:51 +0100

    added myAccount page

--- src/Pages/MyAccount/MyAccount.jsx
+++ src/Pages/MyAccount/MyAccount.jsx
@@ -3,7 +3,19 @@ import './MyAccount.css'
 
 const MyAccount = () => {
   return (
-    <div>MyAccount</div>
+    <div className='accountLogin'>
+      <div className='login'>
+        <h3>Login</h3> 
+      <p>Please login using account detail bellow.</p>
+      <form className='loginForm' action="">
+        <input  type="text" name="" id="loginEmail" placeholder='Email Address' />
+        <input type="password" name="" id="passwordLogin" placeholder='Password' />
+        <a>Forgot your password?</a>
+        <input type="button" id='submitLogin' value="Sign In" />
+        <a href="#">Don’t have an Account?Create account</a>
+      </form>
+      </div>
+    </div>
   )
 }
 
