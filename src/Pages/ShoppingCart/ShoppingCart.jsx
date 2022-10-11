4ffde7e1a4d090b853648444b59f63abb7307d3d
olawale <jao.olaniyi@gmail.com>
2022-10-11 09:17:07 +0100

    carting div updated

--- src/Pages/ShoppingCart/ShoppingCart.jsx
+++ src/Pages/ShoppingCart/ShoppingCart.jsx
@@ -1,43 +1,122 @@
-import React from 'react'
-import './ShoppingCart.css'
-import Cart from '../../Components/Cart/Cart'
-import bagOne from '../../Assets/bag0ne.svg'
-import bagTwo from '../../Assets/bagTwo.svg'
-import bagThree from '../../Assets/bagThree.svg'
-import bagFour from '../../Assets/bagFour.svg'
-import bagFive from '../../Assets/bagFive.svg'
-import CartTotal from '../../Components/CartTotal/CartTotal'
-
+import React from "react";
+import "./ShoppingCart.css";
+import Cart from "../../Components/Cart/Cart";
+import bagOne from "../../Assets/bag0ne.svg";
+import bagTwo from "../../Assets/bagTwo.svg";
+import bagThree from "../../Assets/bagThree.svg";
+import bagFour from "../../Assets/bagFour.svg";
+import bagFive from "../../Assets/bagFive.svg";
+import CartTotal from "../../Components/CartTotal/CartTotal";
 
 const ShoppingCart = () => {
-  const cartData= [
-    {id:1,heading:'dkv', logo:bagOne, name:'Ut diam consequat',color:'Color:Brown', size:'Size:XL', price:'$32.00', decrease:'-', count:'1', increase:'+', total:'£219.00'},
-    {id:2, logo:bagTwo, name:'Vel faucibus posuere',color:'Color:Brown', size:'Size:XL', price:'$32.00', decrease:'-', count:'1', increase:'+', total:'£219.00'},
-    {id:3, logo:bagThree, name:'Ac vitae vestibulum',color:'Color:Brown', size:'Size:XL', price:'$32.00', decrease:'-', count:'1', increase:'+', total:'£219.00'},
-    {id:4, logo:bagFour, name:'Elit massa diam',color:'Color:Brown', size:'Size:XL', price:'$32.00', decrease:'-', count:'1', increase:'+', total:'£219.00'},
-    {id:5, logo:bagFive, name:'Proin pharetra elementum',color:'Color:Brown', size:'Size:XL', price:'$32.00', decrease:'-', count:'1', increase:'+', total:'£219.00'}
-
-  ]
+  const cartData = [
+    {
+      id: 1,
+      // heading: "Product",
+      logo: bagOne,
+      name: "Ut diam consequat",
+      color: "Color:Brown",
+      size: "Size:XL",
+      price: "$32.00",
+      decrease: "-",
+      count: "1",
+      increase: "+",
+      total: "£219.00",
+    },
+    {
+      id: 2,
+      // heading: "Product",
+      logo: bagTwo,
+      name: "Vel faucibus posuere",
+      color: "Color:Brown",
+      size: "Size:XL",
+      price: "$32.00",
+      decrease: "-",
+      count: "1",
+      increase: "+",
+      total: "£219.00",
+    },
+    {
+      id: 3,
+      // heading: "Product",
+      logo: bagThree,
+      name: "Ac vitae vestibulum",
+      color: "Color:Brown",
+      size: "Size:XL",
+      price: "$32.00",
+      decrease: "-",
+      count: "1",
+      increase: "+",
+      total: "£219.00",
+    },
+    {
+      id: 4,
+      // heading: "Product",
+      logo: bagFour,
+      name: "Elit massa diam",
+      color: "Color:Brown",
+      size: "Size:XL",
+      price: "$32.00",
+      decrease: "-",
+      count: "1",
+      increase: "+",
+      total: "£219.00",
+    },
+    {
+      id: 5,
+      // heading: "Product",
+      logo: bagFive,
+      name: "Proin pharetra elementum",
+      color: "Color:Brown",
+      size: "Size:XL",
+      price: "$32.00",
+      decrease: "-",
+      count: "1",
+      increase: "+",
+      total: "£219.00",
+    },
+  ];
 
   return (
-    <div className='shopCart'>
-
-<div className='cartMap'>
-        {
-          cartData.map(items=>(
-            <Cart heading={items.heading} img={items.logo} name={items.name} color={items.color} size={items.size} price={items.price} decrease={items.decrease} count={items.count} increase={items.increase} total={items.total} key={items.id}/>
-          ))
-        }
-        <div className='btncartUpdate'>
-        <button id='btncart' type="submit">Update Curt</button>
-       <button id='btncart' type="submit">Clear Curt</button>
+    <div className="cartDiv">
+      <div className="heading">
+        <h3 id="h3product">Product</h3>
+        <h3 id="h3price">Price</h3>
+        <h3 id="h3quantity">Quantity</h3>
+        <h3 id="h3total">Total</h3>
+      </div>
+      <div className="shopCart">
+        <div className="cartMap">
+          {cartData.map((items) => (
+            <Cart
+              // heading={items.heading}
+              img={items.logo}
+              name={items.name}
+              color={items.color}
+              size={items.size}
+              price={items.price}
+              decrease={items.decrease}
+              count={items.count}
+              increase={items.increase}
+              total={items.total}
+              key={items.id}
+            />
+          ))}
+          <span className="btncartUpdate">
+            <button id="btncart" type="submit">
+              Update Curt
+            </button>
+            <button id="btncart" type="submit">
+              Clear Curt
+            </button>
+          </span>
+        </div>
+        <div>
+          <CartTotal />
         </div>
       </div>
-      <div>
-      <CartTotal/>
-      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShoppingCart
+export default ShoppingCart;
